f5c251987d4b07d2a62b4e9b3f2f84c17121ca94
Raven Crowe <ravencrowe01@gmail.com>
2023-06-04 18:24:26 -0400

    Hide chunk validator setup, Namespaces with no brackets scare me

--- librbr.Tests/ChunkBuilderTests.cs
+++ librbr.Tests/ChunkBuilderTests.cs
@@ -1,46 +1,47 @@
-namespace librbr.Tests;
+namespace librbr.Tests {
+    public class ChunkBuilderTests {
+        private int _seedA = 42069;
 
-public class ChunkBuilderTests {
-    private int _seedA = 42069;
+        [Test]
+        public void ChunkBuilder_SeedA_Valid ( ) {
+            var builder = new ChunkBuilder(new RNGProvider(_seedA), _seedA);
 
-    [Test]
-    public void ChunkBuilder_SeedA_Valid() {
-        var builder = new ChunkBuilder(new RNGProvider(_seedA), _seedA);
+            builder.WithCoordinates(new Coordinate(0, 0)).WithDimensions(3);
 
-        builder.WithCoordinates(new Coordinate(0, 0)).WithDimensions(3);
+            var chunk = builder.BuildChunk(new ChunkValidator());
 
-        var chunk = builder.BuildChunk(new ChunkValidator());
+            // System.Console.WriteLine((chunk as ProtoChunk).ToString());
 
-        // System.Console.WriteLine((chunk as ProtoChunk).ToString());
-
-        Assert.That(chunk, Is.Not.Null);
-    }
-
-    private class RNGProvider : IRNGProvider {
-        private Random rng;
-
-        public RNGProvider (int seed) {
-            rng = new Random(seed);
+            Assert.That(chunk, Is.Not.Null);
         }
 
-        public int Range (int max) => rng.Next(max + 1);
+        private class RNGProvider : IRNGProvider {
+            private Random rng;
 
-        public int Range (int min, int max) => rng.Next(min, max + 1);
-
-        public float Range (float max) => (float) (rng.NextDouble() * max);
-
-        public float Range (float min, float max) {
-            float roll = (float) (rng.NextDouble() * max);
-
-            while(roll < min) {
-                roll = (float) (rng.NextDouble() * max);
+            public RNGProvider (int seed) {
+                rng = new Random(seed);
             }
 
-            return roll;
-        }
+            public int Range (int max) => rng.Next(max + 1);
 
-        public void SetSeed (int seed) {
-            rng = new Random(seed);
+            public int Range (int min, int max) => rng.Next(min, max + 1);
+
+            public float Range (float max) => (float) (rng.NextDouble() * max);
+
+            public float Range (float min, float max) {
+                float roll = (float) (rng.NextDouble() * max);
+
+                while (roll < min) {
+                    roll = (float) (rng.NextDouble() * max);
+                }
+
+                return roll;
+            }
+
+            public void SetSeed (int seed) {
+                rng = new Random(seed);
+            }
         }
     }
 }
+
--- librbr.Tests/ChunkValidatorTests.cs
+++ librbr.Tests/ChunkValidatorTests.cs
@@ -1,101 +1,19 @@
-namespace librbr.Tests;
+namespace librbr.Tests {
+    public partial class ChunkValidatorTests {
+        private ProtoChunk _chunkA;
 
-public class ChunkValidatorTests {
-    private ProtoChunk _chunkA;
+        [SetUp]
+        public void Setup ( ) {
+            SetupChunkA();
+        }
 
-    [SetUp]
-    public void Setup ( ) {
-        SetupChunkA();
-    }
+        [Test]
+        public void ChunkValidator_ChunkA_Valid ( ) {
+            var val = new ChunkValidator();
 
-    [Test]
-    public void ChunkValidator_ChunkA_Valid ( ) {
-        var val = new ChunkValidator();
+            // System.Console.WriteLine(_chunkA.ToString());
 
-        // System.Console.WriteLine(_chunkA.ToString());
-
-        Assert.That(val.ValidateChunk(_chunkA), Is.EqualTo(true));
-    }
-
-    private void SetupChunkA ( ) {
-        _chunkA = new ProtoChunk(4, new Coordinate(0, 0));
-
-        var r00 = new ProtoRoom(new Coordinate(0, 0));
-        r00.OpenSide(Direction.North);
-        r00.OpenSide(Direction.East);
-        _chunkA.SetRoom(new Coordinate(0, 0), r00);
-
-        var r10 = new ProtoRoom(new Coordinate(1, 0));
-        r10.SetSideState(Direction.West, true);
-        _chunkA.SetRoom(new Coordinate(1, 0), r10);
-
-        var r20 = new ProtoRoom(new Coordinate(2, 0));
-        r20.OpenSide(Direction.North);
-        _chunkA.SetRoom(new Coordinate(2, 0), r20);
-
-        var r30 = new ProtoRoom(new Coordinate(3, 0));
-        r30.OpenSide(Direction.North);
-        _chunkA.SetRoom(new Coordinate(3, 0), r30);
-
-        var r01 = new ProtoRoom(new Coordinate(0, 1));
-        r01.OpenSide(Direction.East);
-        r01.OpenSide(Direction.South);
-        _chunkA.SetRoom(new Coordinate(0, 1), r01);
-
-        var r11 = new ProtoRoom(new Coordinate(1, 1));
-        r11.OpenSide(Direction.North);
-        r11.OpenSide(Direction.East);
-        r11.OpenSide(Direction.West);
-        _chunkA.SetRoom(new Coordinate(1, 1), r11);
-
-        var r21 = new ProtoRoom(new Coordinate(2, 1));
-        r21.OpenSide(Direction.South);
-        r21.OpenSide(Direction.East);
-        r21.OpenSide(Direction.West);
-        _chunkA.SetRoom(new Coordinate(2, 1), r21);
-
-        var r31 = new ProtoRoom(new Coordinate(3, 1));
-        r31.OpenSide(Direction.North);
-        r31.OpenSide(Direction.South);
-        r31.OpenSide(Direction.West);
-        _chunkA.SetRoom(new Coordinate(3, 1), r31);
-
-        var r02 = new ProtoRoom(new Coordinate(0, 2));
-        r02.OpenSide(Direction.East);
-        _chunkA.SetRoom(new Coordinate(0, 2), r02);
-
-        var r12 = new ProtoRoom(new Coordinate(1, 2));
-        r12.OpenSide(Direction.North);
-        r12.OpenSide(Direction.South);
-        r12.OpenSide(Direction.West);
-        _chunkA.SetRoom(new Coordinate(1, 2), r12);
-
-        var r22 = new ProtoRoom(new Coordinate(2, 2));
-        r22.OpenSide(Direction.North);
-        r22.OpenSide(Direction.East);
-        _chunkA.SetRoom(new Coordinate(2, 2), r22);
-
-        var r32 = new ProtoRoom(new Coordinate(3, 2));
-        r32.OpenSide(Direction.South);
-        r32.OpenSide(Direction.West);
-        _chunkA.SetRoom(new Coordinate(3, 2), r32);
-
-        var r03 = new ProtoRoom(new Coordinate(0, 3));
-        r03.OpenSide(Direction.East);
-        _chunkA.SetRoom(new Coordinate(0, 3), r03);
-
-        var r13 = new ProtoRoom(new Coordinate(1, 3));
-        r13.OpenSide(Direction.South);
-        r13.OpenSide(Direction.West);
-        _chunkA.SetRoom(new Coordinate(1, 3), r13);
-
-        var r23 = new ProtoRoom(new Coordinate(2, 3));
-        r23.OpenSide(Direction.South);
-        r23.OpenSide(Direction.East);
-        _chunkA.SetRoom(new Coordinate(2, 3), r23);
-
-        var r33 = new ProtoRoom(new Coordinate(3, 3));
-        r33.OpenSide(Direction.West);
-        _chunkA.SetRoom(new Coordinate(3, 3), r33);
+            Assert.That(val.ValidateChunk(_chunkA), Is.EqualTo(true));
+        }
     }
 }
